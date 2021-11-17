3d889b54ed0162e8e2301aaec9d96eead6129e9e
MManahan <mermanahan@gmail.com>
2021-11-17 21:51:58 +0000

    added snapshots to project

---
+++ dbt-greenery/snapshots/snap_users.sql
@@ -0,0 +1,16 @@
+{% snapshot snap_users %}
+
+{{
+    config(
+      target_database='dbt',
+      target_schema='snapshots',
+      unique_key='user_id',
+
+      strategy='timestamp',
+      updated_at='updated_at',
+    )
+}}
+
+select * from {{ref('stg_users_table')}}
+
+{% endsnapshot %}
