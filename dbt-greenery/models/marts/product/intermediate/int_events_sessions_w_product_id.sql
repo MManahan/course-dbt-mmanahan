fa10544a81b422d4fe85e5da4e2f0143a3fc94a5
MManahan <mermanahan@gmail.com>
2021-12-01 22:10:39 +0000

    table providing sessions with product_id

--- dbt-greenery/models/marts/product/intermediate/int_events_sessions_w_product_id.sql
+++ dbt-greenery/models/marts/product/intermediate/int_events_sessions_w_product_id.sql
@@ -9,10 +9,28 @@ with session_w_product_id as (
     select * from {{ref('stg_events_table')}}
 
     )
-
+,
+sessions_w_product_id as (
 select 
       session_id,
-      coalesce(product_id,'no product') as product_id
+      product_id
 
 from 
       session_w_product_id
+group by 
+      session_id,
+      product_id
+order by 
+      session_id
+)
+select 
+      s.session_id,
+      s.product_id,
+      p.name
+
+from sessions_w_product_id as s
+join {{ref('stg_products_table')}} as p 
+on
+      s.product_id = p.product_id
+
+
