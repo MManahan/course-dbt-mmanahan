2f60cc3ccfbc928bb6839be201c881076e2d23a5
MManahan <mermanahan@gmail.com>
2021-12-01 01:20:30 +0000

    added new int model to group sessions and count by event

---
+++ dbt-greenery/models/marts/product/intermediate/int_sessions__grouped.sql
@@ -0,0 +1,26 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+
+with events as (
+
+    select * from {{ref('stg_events_table')}}
+
+    )
+
+select 
+      session_id,
+      sum(CASE WHEN event_type = 'add_to_cart' then 1 ELSE NULL END) as Add_to_Cart,
+      sum(CASE WHEN event_type = 'checkout' then 1 ELSE NULL END) as Checkout,
+      sum(CASE WHEN event_type = 'delete_from_cart' then 1 ELSE NULL END) as Delete_from_Cart,
+      sum(CASE WHEN event_type = 'package_shipped' then 1 ELSE NULL END) as Package_Shipped,
+      sum(CASE WHEN event_type = 'page_view' then 1 ELSE NULL END) as Page_View,
+      sum(CASE WHEN event_type = 'account_created' then 1 ELSE NULL END) as Account_Created
+
+from 
+      events
+
+group by 
+      session_id
