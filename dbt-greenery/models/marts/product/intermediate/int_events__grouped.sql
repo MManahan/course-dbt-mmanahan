62372687a8367210d6ab5723058ce902d181a935
MManahan <mermanahan@gmail.com>
2021-12-02 21:20:12 +0000

    changed agg from count to sum

--- dbt-greenery/models/marts/product/intermediate/int_events__grouped.sql
+++ dbt-greenery/models/marts/product/intermediate/int_events__grouped.sql
@@ -13,12 +13,12 @@ with events as (
 
 select 
       user_id,
-      COUNT(CASE WHEN event_type = 'add_to_cart' then 1 ELSE NULL END) as Add_to_Cart,
-      COUNT(CASE WHEN event_type = 'checkout' then 1 ELSE NULL END) as Checkout,
-      COUNT(CASE WHEN event_type = 'delete_from_cart' then 1 ELSE NULL END) as Delete_from_Cart,
-      COUNT(CASE WHEN event_type = 'package_shipped' then 1 ELSE NULL END) as Package_Shipped,
-      COUNT(CASE WHEN event_type = 'page_view' then 1 ELSE NULL END) as Page_View,
-      COUNT(CASE WHEN event_type = 'account_created' then 1 ELSE NULL END) as Account_Created
+      sum(CASE WHEN event_type = 'add_to_cart' then 1 ELSE 0 END) as Add_to_Cart,
+      sum(CASE WHEN event_type = 'checkout' then 1 ELSE 0 END) as Checkout,
+      sum(CASE WHEN event_type = 'delete_from_cart' then 1 ELSE 0 END) as Delete_from_Cart,
+      sum(CASE WHEN event_type = 'package_shipped' then 1 ELSE 0 END) as Package_Shipped,
+      sum(CASE WHEN event_type = 'page_view' then 1 ELSE 0 END) as Page_View,
+      sum(CASE WHEN event_type = 'account_created' then 1 ELSE 0 END) as Account_Created
 
 from 
       events
