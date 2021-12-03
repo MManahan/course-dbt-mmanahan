48b1c6229993e4cd281f4e50c00f6cdc89115cb9
MManahan <mermanahan@gmail.com>
2021-12-03 17:47:56 +0000

    trying to get distinc_col macro to work

---
+++ dbt-greenery/macros/distinct_col.sql
@@ -0,0 +1,27 @@
+{% macro get_column_values(column_name, relation) %}
+
+{% set relation_query %}
+select distinct
+{{ column_name }}
+from {{ relation }}
+order by 1
+{% endset %}
+
+{% set results = run_query(relation_query) %}
+
+{% if execute %}
+{# Return the first column #}
+{% set results_list = results.columns[0].values() %}
+{% else %}
+{% set results_list = [] %}
+{% endif %}
+
+{% set results = (results_list) %}
+
+{% for result in results_list %}
+    , sum(case when relation.event_type = result then
+    1 else 0 end) as {{result}}
+{% endfor %}
+
+{% endmacro %}
+
--- dbt-greenery/models/marts/product/intermediate/int_events_grouped_by_session.sql
+++ dbt-greenery/models/marts/product/intermediate/int_events_grouped_by_session.sql
@@ -12,12 +12,13 @@ with events as (
 
 select 
       session_id,
-      sum(CASE WHEN event_type = 'add_to_cart' then 1 ELSE NULL END) as Add_to_Cart,
-      sum(CASE WHEN event_type = 'checkout' then 1 ELSE NULL END) as Checkout,
-      sum(CASE WHEN event_type = 'delete_from_cart' then 1 ELSE NULL END) as Delete_from_Cart,
-      sum(CASE WHEN event_type = 'package_shipped' then 1 ELSE NULL END) as Package_Shipped,
-      sum(CASE WHEN event_type = 'page_view' then 1 ELSE NULL END) as Page_View,
-      sum(CASE WHEN event_type = 'account_created' then 1 ELSE NULL END) as Account_Created
+      {{get_column_values('event_type', {{ref('stg_events_table')}})}}
+--      sum(CASE WHEN event_type = 'add_to_cart' then 1 ELSE NULL END) as Add_to_Cart,
+--      sum(CASE WHEN event_type = 'checkout' then 1 ELSE NULL END) as Checkout,
+--      sum(CASE WHEN event_type = 'delete_from_cart' then 1 ELSE NULL END) as Delete_from_Cart,
+--      sum(CASE WHEN event_type = 'package_shipped' then 1 ELSE NULL END) as Package_Shipped,
+--      sum(CASE WHEN event_type = 'page_view' then 1 ELSE NULL END) as Page_View,
+--      sum(CASE WHEN event_type = 'account_created' then 1 ELSE NULL END) as Account_Created
 
 from 
       events
