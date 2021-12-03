bf42f8909627d7f37f5cc40d4a4751b85995f8e2
MManahan <mermanahan@gmail.com>
2021-12-03 19:07:51 +0000

    deleted macro and added jinja and used dbt.utils.get_column_values

--- dbt-greenery/models/marts/product/intermediate/int_events_grouped_by_session.sql
+++ dbt-greenery/models/marts/product/intermediate/int_events_grouped_by_session.sql
@@ -4,6 +4,11 @@
   )
 }}
 
+{%- set event_types = dbt_utils.get_column_values(
+    table=ref('stg_events_table'),
+    column='event_type'
+) -%}
+
 with events as (
 
     select * from {{ref('stg_events_table')}}
@@ -12,7 +17,12 @@ with events as (
 
 select 
       session_id,
-      {{get_column_values('event_type', ref('stg_events_table'))}}
+      {%- for event_type in event_types %}
+      count(case when event_type = '{{event_type}}' then event_id end) as count_of_{{event_type}}
+      {%- if not loop.last %},{% endif -%}
+      {% endfor %}
+
+--    The code below was replaced with the jinja above utilizing dbt.utils.get_column_values:
 --      sum(CASE WHEN event_type = 'add_to_cart' then 1 ELSE NULL END) as Add_to_Cart,
 --      sum(CASE WHEN event_type = 'checkout' then 1 ELSE NULL END) as Checkout,
 --      sum(CASE WHEN event_type = 'delete_from_cart' then 1 ELSE NULL END) as Delete_from_Cart,
