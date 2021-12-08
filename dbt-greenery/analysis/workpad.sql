39fcc5d4baf4d104301a7157e216231af0b02fad
MManahan <mermanahan@gmail.com>
2021-12-08 18:02:34 +0000

    added query for last question

--- dbt-greenery/analysis/workpad.sql
+++ dbt-greenery/analysis/workpad.sql
@@ -1,31 +1,25 @@
--- Rate of Repeat Users 
 
-with user_orders as (
-select 
-        u.user_id, 
-        count(o.order_id) as count_of_orders 
+with agg_table as (
 
-from 
-        "stg_users_table" as u
-
-left join    "stg_orders_table" as o
-
-on      u.user_id = o.user_id
-
-group by u.user_idå
-
-having count(o.order_id) > 0
-),
-
-repeat_users as (
-
-select 
-        count(case when count_of_orders > 1 then 1 else null end) as count_repeat,
-        count(case when count_of_orders > 0 then 1 else null end) as count_all
+    SELECT
+        session_id
+         , count(case when event_type = 'page_view' then 1 else 0 end) as has_page_view
+         , count(case when event_type = 'add_to_cart' then 1 else 0 end) as has_add_to_cart
+         , count(case when event_type = 'checkout' then 1 else 0 end) as has_checkout
 FROM
-        user_orders
+    "stg_events_table"
+GROUP BY
+    session_id
+
+),
+bool_table as ( 
+
+SELECT
+    count(session_id) as num_sessions
+     , count(case when has_page_view > 0 then 1 else 0 end) as num_pv
+     , count(case when has_add_to_cart > 0 then 1 else 0 end) as num_atc
+     , count(case when has_checkout > 0 then 1 else 0 end) as num_cout
+FROM
+    agg_table
 )
-
-select round((count_repeat::decimal / count_all)*100,1) from repeat_users
-
-
+select * from bool_table
