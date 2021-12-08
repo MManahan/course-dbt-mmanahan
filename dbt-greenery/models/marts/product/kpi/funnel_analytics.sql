af5150e4635d856e240be2b445472be20306d869
MManahan <mermanahan@gmail.com>
2021-12-08 19:16:29 +0000

    moved 2 models to new kpi folder and made new model for funnel_analytics

---
+++ dbt-greenery/models/marts/product/kpi/funnel_analytics.sql
@@ -0,0 +1,42 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+
+with bool_table as (
+
+    SELECT
+        session_id
+         , case when event_type = 'page_view' then 1 else 0 end as has_page_view
+         , case when event_type = 'add_to_cart' then 1 else 0 end as has_add_to_cart
+         , case when event_type = 'checkout' then 1 else 0 end as has_checkout
+    FROM
+    {{ref('stg_events_table')}}
+
+),
+agg_table as ( 
+
+    SELECT
+        session_id
+         , sum(has_page_view) as num_pv
+         , sum(has_add_to_cart) as num_atc
+         , sum(has_checkout) as num_cout
+    FROM
+        bool_table
+    GROUP BY  
+        session_id
+ ),
+
+final_table as (
+    
+    SELECT
+        count(session_id) as num_sessions
+         , sum(case when num_pv > 0 then 1 else 0 end) as page_views
+         , sum(case when num_atc > 0 then 1 else 0 end) as add_to_carts
+         , sum(case when num_cout > 0 then 1 else 0 end) as checkouts
+    FROM
+        agg_table
+)
+select * from final_table
