d55661c4c1781a60895ce51f20d82b965cb05dd1
MManahan <mermanahan@gmail.com>
2021-12-08 18:14:23 +0000

    update query

--- dbt-greenery/analysis/workpad.sql
+++ dbt-greenery/analysis/workpad.sql
@@ -1,25 +1,25 @@
 
-with agg_table as (
+with bool_table as (
 
     SELECT
         session_id
-         , count(case when event_type = 'page_view' then 1 else 0 end) as has_page_view
-         , count(case when event_type = 'add_to_cart' then 1 else 0 end) as has_add_to_cart
-         , count(case when event_type = 'checkout' then 1 else 0 end) as has_checkout
-FROM
+         , case when event_type = 'page_view' then 1 else 0 end as has_page_view
+         , case when event_type = 'add_to_cart' then 1 else 0 end as has_add_to_cart
+         , case when event_type = 'checkout' then 1 else 0 end as has_checkout
+    FROM
     "stg_events_table"
-GROUP BY
-    session_id
 
 ),
-bool_table as ( 
+agg_table as ( 
 
-SELECT
-    count(session_id) as num_sessions
-     , count(case when has_page_view > 0 then 1 else 0 end) as num_pv
-     , count(case when has_add_to_cart > 0 then 1 else 0 end) as num_atc
-     , count(case when has_checkout > 0 then 1 else 0 end) as num_cout
-FROM
-    agg_table
+    SELECT
+        session_id
+         , sum(has_page_view) as num_pv
+         , sum(has_add_to_cart) as num_atc
+         , sum(has_checkout) as num_cout
+    FROM
+        bool_table
+    GROUP BY  
+        session_d
 )
-select * from bool_table
+select * from agg_table
